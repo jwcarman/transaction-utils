d567347c9cc1ab937237ec53b5d6354455882882
James Carman <james@carmanconsulting.com>
2012-08-16 07:42:47 -0400

    Adding JTA transaction manager test.

--- src/test/java/com/carmanconsulting/spring/tx/TestChainedTransactionManager.java
+++ src/test/java/com/carmanconsulting/spring/tx/TestChainedTransactionManager.java
@@ -1,23 +1,34 @@
 package com.carmanconsulting.spring.tx;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.commons.dbcp.BasicDataSource;
+import org.apache.commons.dbcp.managed.BasicManagedDataSource;
 import org.h2.Driver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.objectweb.jotm.Jotm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowCountCallbackHandler;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jms.connection.JmsTransactionManager;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.jta.JtaTransactionManager;
 import org.springframework.transaction.support.TransactionCallback;
 import org.springframework.transaction.support.TransactionTemplate;
 
+import javax.jms.ConnectionFactory;
+import javax.jms.Message;
+import javax.sql.DataSource;
+import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class TestChainedTransactionManager
 {
@@ -26,13 +37,13 @@ public class TestChainedTransactionManager
 //----------------------------------------------------------------------------------------------------------------------
 
     private static final Logger logger = LoggerFactory.getLogger(TestChainedTransactionManager.class);
+    public static final String TEST_QUEUE = "testQueue";
 
     private BasicDataSource ds1;
     private BasicDataSource ds2;
-    private DataSourceTransactionManager tx1;
-    private DataSourceTransactionManager tx2;
     private BasicDataSource ds3;
-    private DataSourceTransactionManager tx3;
+    private Jotm jotm;
+    private ConnectionFactory connectionFactory;
 
 //----------------------------------------------------------------------------------------------------------------------
 // Other Methods
@@ -59,53 +70,117 @@ public class TestChainedTransactionManager
     }
 
     @Before
-    public void setUpConnections()
+    public void setUpConnections() throws Exception
     {
+        jotm = new Jotm(true, false);
+        connectionFactory = new ActiveMQConnectionFactory("vm:" + getClass().getSimpleName() + "?persistent=false&useJmx=false");
         ds1 = createDataSource();
         ds2 = createDataSource();
         ds3 = createDataSource();
-        tx1 = new DataSourceTransactionManager(ds1);
-        tx2 = new DataSourceTransactionManager(ds2);
-        tx3 = new DataSourceTransactionManager(ds3);
     }
 
     private BasicDataSource createDataSource()
     {
-        BasicDataSource ds = new BasicDataSource();
+        BasicManagedDataSource ds = new BasicManagedDataSource();
         ds.setDriverClassName(Driver.class.getName());
         ds.setUrl("jdbc:h2:mem:" + UUID.randomUUID().toString());
         ds.setUsername("sa");
         ds.setPassword("");
+        ds.setTransactionManager(jotm.getTransactionManager());
         JdbcTemplate template = new JdbcTemplate(ds);
         template.update("create table test_table (id int, value varchar(255))");
         return ds;
     }
 
+    private void executeUpdate(String sql, DataSource dataSource)
+    {
+        Connection connection = null;
+        Statement statement = null;
+        try
+        {
+            connection = dataSource.getConnection();
+            statement = connection.createStatement();
+            statement.executeUpdate(sql);
+        }
+        catch(SQLException e)
+        {
+            throw new RuntimeException("Unable to execute update.", e);
+        }
+        finally
+        {
+            closeStatement(statement);
+            closeConnection(connection);
+        }
+    }
+
+    private void closeConnection(Connection connection)
+    {
+        try
+        {
+            if(connection != null)
+            {
+                connection.close();
+            }
+        }
+        catch(SQLException e)
+        {
+            logger.error("Unable to close connection.", e);
+        }
+    }
+    private void closeStatement(Statement statement)
+    {
+        try
+        {
+            if(statement != null)
+            {
+                statement.close();
+            }
+        }
+        catch(SQLException e)
+        {
+            logger.error("Unable to close statement.", e );
+        }
+    }
+
+    @Before
+    public void setUpJtaTransactionManager() throws Exception
+    {
+
+    }
+
     @Test
     public void testDistributedTransactionRollback()
     {
-        final ChainedTransactionManager transactionManager = new ChainedTransactionManager(tx1, tx2, tx3);
+        final JtaTransactionManager jtaTransactionManager = new JtaTransactionManager(jotm.getTransactionManager());
+        final JmsTransactionManager jmsTransactionManager = new JmsTransactionManager(connectionFactory);
+        final ChainedTransactionManager transactionManager = new ChainedTransactionManager(jtaTransactionManager, jmsTransactionManager);
         TransactionTemplate txTemplate = new TransactionTemplate(transactionManager);
+
         try
         {
-            txTemplate.execute(new TransactionCallback<Void>()
-            {
-                @Override
-                public Void doInTransaction(TransactionStatus transactionStatus)
-                {
-                    new JdbcTemplate(ds1).update("insert into test_table values (1,'hello')");
-                    new JdbcTemplate(ds2).update("insert into test_table values (2)");
-                    return null;
-                }
-            });
-        }
-        catch (RuntimeException e)
+        txTemplate.execute(new TransactionCallback<Void>()
         {
-            logger.info("Encountered error.", e);
-        }
+            @Override
+            public Void doInTransaction(TransactionStatus status)
+            {
+                new JmsTemplate(connectionFactory).convertAndSend("testQueue", "Hello, World!");
+                executeUpdate("insert into test_table values (1, 'hello')", ds1);
+                executeUpdate("insert into test_table values (2)", ds2);
 
+                return null;
+            }
+        });
+        }
+        catch(RuntimeException e)
+        {
+            logger.error("Caught exception!", e);
+        }
         RowCountCallbackHandler handler = new RowCountCallbackHandler();
         new JdbcTemplate(ds1).query("select * from test_table", handler);
         assertEquals(0, handler.getRowCount());
+        JmsTemplate template = new JmsTemplate(connectionFactory);
+        template.setReceiveTimeout(250);
+        Message message = template.receive(TEST_QUEUE);
+        assertNull(message);
     }
 }
